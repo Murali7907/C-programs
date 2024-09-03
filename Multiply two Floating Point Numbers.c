298be4d5bc0d557c763614fad07eea73463a8845
Murali7907 <144107914+Murali7907@users.noreply.github.com>
2024-09-03 14:46:28 +0530

    Create Multiply two Floating Point Numbers.c

---
+++ Multiply two Floating Point Numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float num1,num2,result;
+    printf("enter 2 number");
+    scanf("%f %f",&num1,&num2);
+    result=num1*num2;
+    printf("the result:%f",result);
+    
+}
