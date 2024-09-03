849be24ad9db4836cf7f7fbecc1917a197103a8b
Murali7907 <144107914+Murali7907@users.noreply.github.com>
2024-09-03 14:03:51 +0530

    Create Add Two Numbers.c

---
+++ Add Two Numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+  int num1,num2,sum;
+printf("enter 2 number :");
+scanf("%d %d",&num1,&num2);
+sum=num1+num2;
+printf("the sum of %d and %d is : %d",num1,num2,sum);
+}
