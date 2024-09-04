2d1947564265e7b7413bc9119408706320283607
Murali7907 <144107914+Murali7907@users.noreply.github.com>
2024-09-04 08:17:35 +0530

    Create  Size of int, float, double, and char.c

---
+++ Size of int, float, double, and char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+ int i;
+ char c;
+ float f;
+ double d;
+ printf("the size of int : %u \n",sizeof(i));
+ printf("the size of char : %u \n",sizeof(c));
+ printf("the size of float : %u \n",sizeof(f));
+ printf("the size of double : %u \n",sizeof(d));
+
+    return 0;
+}
