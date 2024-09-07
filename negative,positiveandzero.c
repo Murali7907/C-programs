faca2ce0afc289f66951a38ca3a92151588d8dd4
Murali7907 <144107914+Murali7907@users.noreply.github.com>
2024-09-07 20:23:32 +0530

    Create negative,positiveandzero.c

---
+++ negative,positiveandzero.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("enter the nuber\n");
+    scanf("%d",&number;
+    if(number==0)
+{
+    printf("the number is zero");
+}
+   else if(number<0)
+    {
+        printf("\nthe number is negative");
+    }
+    else
+    printf("\nthe number is positive");
+    return 0;
+}
