04c2af060dab98452f0150a81b9f3d848193f76b
Murali7907 <144107914+Murali7907@users.noreply.github.com>
2024-09-22 19:48:09 +0530

    MaxAndMin.c

--- MaxAndMin.c
+++ MaxAndMin.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main() {
-  int n;
+  int size;
   double arr[100];
-  printf("Enter the number of elements (1 to 100): ");
-  scanf("%d", &n);
+  printf("Enter the size of the array ");
+  scanf("%d", &size);
 
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < size; ++i) {
     printf("Enter number%d: ", i + 1);
     scanf("%lf", &arr[i]);
   }
 
  
-  for (int i = 1; i < n; ++i) {
-    if (arr[0] < arr[i]) {
+  for (int i = 1; i < size; ++i) {
+    if (arr[0] <= arr[i]) {
       arr[0] = arr[i];
     }
   }
@@ -21,13 +21,13 @@ int main() {
 
 
  
-  for (int i = 1; i < n; ++i) {
-    if (arr[0] > arr[i]) {
+  for (int i = 1; i < size; ++i) {
+    if (arr[0] >= arr[i]) {
       arr[0] = arr[i];
     }
   }
 
-  printf("\nMinimum Element= %.2lf", arr[0]);
+  printf("\nMinimum Element = %.2lf", arr[0]);
 
   return 0;
 }
